8ae14275474be10a95a4c9c6863ade64f683158e
RandhirKaushik0908 <randhir.kaushik@my.jcu.edu.au>
2020-08-31 20:47:36 +1000

    Complete debug exercises

--- DebuggingExercises/DebugSeven4.java
+++ DebuggingExercises/DebugSeven4.java
@@ -6,19 +6,19 @@ public class DebugSeven4
    public static void main(String[] args)
    {
       String aString = "HELP!! I need to get 37 things DONE today!!";
+      System.out.println("In all lowercase, the sentence is: \n" + aString);
       int numLetters = 0;
-      int stringLength = aString.length;
+      int stringLength = aString.length();
       System.out.println("In all lowercase, the sentence is: ");
-      for(int i = 0; i < length; i++)
+      for(int i = 0; i < stringLength; i++)
       {
-         char ch = Character.toLowerCase(aString.charAt(length));
+         char ch = Character.toLowerCase(aString.charAt(i));
          System.out.print(ch);
-         if(Character.isLetter())
+         if(Character.isLetter(ch))
             numLetters++;
       }
       System.out.println();
-      System.out.println
-         ("The number of CHARACTERS in the string is " + stringLength);
-      System.out.println("The number of LETTERS is " + stringLength);
+      System.out.println("The number of CHARACTERS in the string is " + stringLength);
+      System.out.println("The number of LETTERS is " + numLetters);
    }
 }
